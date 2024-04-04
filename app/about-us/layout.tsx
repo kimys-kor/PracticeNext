832bb291c5e5697bf757283cfcec1251efa203ee
kimys <ys2life1@naver.com>
2024-04-05 08:42:41 +0900

    first

---
+++ app/about-us/layout.tsx
@@ -0,0 +1,12 @@
+export default function AboutUsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      {children}
+      &copyright kim joo young
+    </div>
+  );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+import Navigation from "../components/navigation";
+
+export const metadata: Metadata = {
+  title: "Not Found",
+};
+
+export default function NotFound() {
+  return (
+    <div>
+      <Navigation />
+      <h1>Not Found!</h1>
+    </div>
+  );
+}
